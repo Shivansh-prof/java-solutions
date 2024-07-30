80f15b59568a2ccd8d7b276f36713de20953cbf4
Shivansh-prof <prof.shivansh@gmail.com>
2024-07-30 17:13:27 +0530

    N Forest Pattern

--- NForest.java
+++ NForest.java
@@ -1,3 +1,17 @@
+// Problem statement
+// Sam is making a forest visualizer. An N-dimensional forest is represented by the pattern of size NxN filled with ‘*’.
+
+// For every value of ‘N’, help sam to print the corresponding N-dimensional forest.
+
+// Example:
+// Input: ‘N’ = 3
+
+// Output: 
+// * * *
+// * * *
+// * * *
+
+
 public class NForest {
     public static void nForest(int n) {
         // Write your code here
@@ -9,7 +23,7 @@ public class NForest {
         }
     }
     public static void main(String[] args) {
-      int n=4;
+      int n=3;
       nForest(n);
     }
 }
