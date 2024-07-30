2ff1910a313a3b9900e18820ed50624cd89a8872
Shivansh-prof <prof.shivansh@gmail.com>
2024-07-30 17:15:23 +0530

    helloWorld

--- helloWorld.java
+++ helloWorld.java
@@ -1,15 +1,5 @@
-public class NForest {
-    public static void nForest(int n) {
-        // Write your code here
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                System.out.print("*" + " ");
-            }
-            System.out.println();
-        }
-    }
-    public static void main(String[] args) {
-      int n=4;
-      nForest(n);
-    }
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
 }
