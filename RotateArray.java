764ca737a2ac91a7297050afc1472c6418736a94
Shivansh-prof <prof.shivansh@gmail.com>
2025-07-16 12:33:00 +0530

    Rotate Array by k places ( LeetCode )

--- RotateArray.java
+++ RotateArray.java
@@ -6,6 +6,7 @@ public class RotateArray {
     public static int[] rotateExtraArray(int[] nums, int k, int n) {
         // brute force : using extra array
         int[] arr = new int[n];
+        k = k % n;
         int j = 0;
         for(int i=n-k; i<n; i++){
             arr[j] = nums[i];
