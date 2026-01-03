c70c0dbcc498b9789e4935374320fa6f40344546
Shivansh-prof <prof.shivansh@gmail.com>
2026-01-04 00:36:41 +0530

    Search in Rotated Sorted Array with Duplucates (LeetCode-81)

--- SearchRotatedSortedArrayDuplicates.java
+++ SearchRotatedSortedArrayDuplicates.java
@@ -7,12 +7,12 @@ You must decrease the overall operation steps as much as possible.
 */
 
 public class SearchRotatedSortedArrayDuplicates {
-    public static int searchRotatedSortedDuplicates(int[] nums, int target) {
+    public static boolean searchRotatedSortedDuplicates(int[] nums, int target) {
         int low = 0, high = nums.length-1;
         while (low<=high){
             int mid  = (low + high ) / 2;
             if(nums[mid] == target)
-                return mid;
+                return true;
             if(nums[low] == nums[mid] && nums[mid] == nums[high]){
                 low +=1;
                 high-=1;
@@ -29,12 +29,12 @@ public class SearchRotatedSortedArrayDuplicates {
                     high = mid -1;
             }
         }
-        return -1;
+        return false;
     }
 
     public static void main(String[] args) {
         int[] nums = {3,4,5,6,3,7,0,1,2,3,3};
         int k = 0;
-        System.out.println("found at index: "+searchRotatedSortedDuplicates(nums,k));
+        System.out.println("found : "+searchRotatedSortedDuplicates(nums,k));
     }
 }
